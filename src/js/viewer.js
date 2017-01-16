941c5c7e7fea0e5d7249df02a96c33f5e35304d3
Vincent White <xstrengthofonex@gmail.com>
2017-01-17 01:02:34 +0900

    update index.html

--- src/js/viewer.js
+++ src/js/viewer.js
@@ -1,3 +1,56 @@
 $(document).ready(function(){
 	// alert("Document ready");
+	$("#random-btn").click(function(){
+		window.open("https://en.wikipedia.org/wiki/Special:Random");
+	});
+	$("#submit-btn").click(function(){
+		var searchVal = $("#search-input").val();
+		console.log(searchVal);
+		if (searchVal.length > 0){
+			getWikipediaResults(searchVal);			
+		}
+	});
 });
+
+function getWikipediaResults(searchVal){
+    $.ajax({
+      url: "https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exlimit=max&format=json&exsentences=1&exintro=&explaintext=&generator=search&gsrlimit=10&gsrsearch=" + searchVal,
+      dataType: 'JSONP',
+      type: "get",
+      success: function(results){
+      	if (results.hasOwnProperty("query")){
+      		showResults(results);
+      	} else {
+      		$("#results-section").hide();
+      		$("#error").show();
+      		$("#error").text("No Results Found");
+      	}
+      }
+    });
+}
+
+var searchResult, title, content
+
+function showResults(results){
+	$("#error").hide();
+
+	$("#results-section").empty();
+
+	$.each(results.query.pages, function(id, page){
+		searchResult = $("<div id='"+id+"' class='result-card'></div>")
+		console.log(page);
+		searchResult = searchResult.clone();
+		title = "<h2>"+page.title+"</h2>";
+		content = "<p>"+page.extract+"</p></div>";
+		searchResult.append(title + content);
+		$("#results-section").append(searchResult);
+		$("#results-section").fadeIn('slow');
+	});
+
+	$(".result-card").click(function(){
+		var id = $(this).attr("id");
+		window.open('http://en.wikipedia.org/?curid='+id);
+	});
+}
+
+// href='http://en.wikipedia.org/?curid="+id+"'
